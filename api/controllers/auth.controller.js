0cb6ec379fb656d8c703ba4624acc1f1afeac710
reyhan <reyhantrkkal@gmail.com>
2024-08-20 19:36:36 +0300

    show user listings functionality created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User created successfully");
+    res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
   }
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,14 +5,13 @@ import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
   res.json({
-    message: "Api route is working",
+    message: "Api route is working!",
   });
 };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, "You can only update your own account!"));
-
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -30,7 +29,9 @@ export const updateUser = async (req, res, next) => {
       },
       { new: true }
     );
+
     const { password, ...rest } = updatedUser._doc;
+
     res.status(200).json(rest);
   } catch (error) {
     next(error);
