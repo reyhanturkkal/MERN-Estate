2ba8ef138acc90b521d9546bd484245f7208a32d
reyhan <reyhantrkkal@gmail.com>
2024-08-26 08:16:58 +0300

    pre installation added

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
